9eb5164f943a4ea76ba1ba29d46030863a985e4b
Anuranjan Bose <anuranjan.bose@tothenew.com>
2020-06-12 00:58:25 +0530

    Initial project setup

---
+++ CameraFilter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CameraFilter
+//
+//  Created by Anuranjan Bose on 12/06/20.
+//  Copyright © 2020 Anuranjan Bose. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
