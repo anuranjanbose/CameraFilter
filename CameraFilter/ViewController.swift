db79dfa42cb138769e9d7514111eec7fc763e159
Anuranjan Bose <anuranjan.bose@tothenew.com>
2020-06-13 13:34:09 +0530

    Observable image property subscribed from Image display view

--- CameraFilter/ViewController.swift
+++ CameraFilter/ViewController.swift
@@ -7,16 +7,32 @@
 //
 
 import UIKit
+import RxSwift
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var applyFilterButton: UIButton!
+    
+    let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.prefersLargeTitles = true
         self.applyFilterButton.isHidden = true
         // Do any additional setup after loading the view.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let navC = segue.destination as? UINavigationController,
+            let photoCollectionVC = navC.viewControllers.first as? PhotosCollectionViewController else {
+                return
+        }
+        
+        photoCollectionVC.selectedPhoto.subscribe(onNext: {[weak self] photo in
+            self?.photoImageView.image = photo
+        })
+        .disposed(by: disposeBag)
     }
 
 
