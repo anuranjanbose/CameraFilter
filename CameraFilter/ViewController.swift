803108e027860d5a1a8aabab546ba361273e52f1
Anuranjan Bose <anuranjan.bose@tothenew.com>
2020-06-13 14:02:43 +0530

    Filter image using observables

--- CameraFilter/ViewController.swift
+++ CameraFilter/ViewController.swift
@@ -46,12 +46,26 @@ class ViewController: UIViewController {
     @IBAction func applyFilter() {
         
         guard let sourceImage = self.photoImageView.image else { return }
+        
+        /// Using Closure
+        /*
         FilterService().applyFilter(to: sourceImage) {[weak self] image in
             DispatchQueue.main.async {
                 self?.photoImageView.image = image
             }
-            
+
         }
+        */
+        /// Using Observable
+        FilterService().applyFilter(to: sourceImage)
+            .subscribe(onNext: {[weak self] filteredImage in
+                
+                DispatchQueue.main.async {
+                    self?.photoImageView.image = filteredImage
+                }
+                
+            })
+        .disposed(by: disposeBag)
     }
 
 }
